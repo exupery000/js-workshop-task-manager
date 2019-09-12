807401e6eda30d996da1873d48e32b7291b1ce56
Andrey Prisniak <aprisniak@users.noreply.github.com>
2019-09-12 20:14:35 +0300

    Update module.js

--- module.js
+++ module.js
@@ -11,4 +11,23 @@
 
         form.reset();
     };
+
+    const getUpdatedTemplate = task => `
+        <div class="content">
+            <button class="checkbox"></button>
+            <input type="text" disabled value="${task}" />
+        </div>
+        <div class="actions">
+            <button class="star"></button>
+            <button class="edit"></button>
+            <button class="remove"></button>
+        </div>
+    `;
+
+    const addTask = task => {
+        const tasksDom = document.querySelector('.tasks');
+        const taskDom = document.createElement('li');
+
+        taskDom.innerHTML = getUpdatedTemplate(task);
+    };
 }
