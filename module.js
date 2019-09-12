e2631608d6e556054f154453d5b3f53b76ab8878
Andrey Prisniak <aprisniak@users.noreply.github.com>
2019-09-12 20:07:14 +0300

    Update module.js

--- module.js
+++ module.js
@@ -1,3 +1,14 @@
 {
-    console.log('Hello world!');
+    const form = document.querySelector('.createTaskForm');
+
+    form.onsubmit = event => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const task = formData.get('task');
+
+        console.log(task);
+
+        form.reset();
+    };
 }
